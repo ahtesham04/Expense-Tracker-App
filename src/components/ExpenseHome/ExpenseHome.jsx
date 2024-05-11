6b3b5902c810ae920fc144be6f887e8ba4f4a304
Ahtesham Ansari <ahtesham6867@gmail.com>
2024-05-11 19:07:22 +0530

    expense trend section completed

--- src/components/ExpenseHome/ExpenseHome.jsx
+++ src/components/ExpenseHome/ExpenseHome.jsx
@@ -3,9 +3,15 @@ import React, { useState, useEffect } from "react";
 import "./expense.css";
 import ExpenseCard from "../ExpenseCard/ExpenseCard";
 import Transactions from "../Recent Transaction/Transactions";
+import ExpenseTrend from "../ExpenseTrend/ExpenseTrend";
+import { useSnackbar } from "notistack";
 
 // localStorage.setItem("expences", JSON.stringify(transactionItem));
-localStorage.setItem("balance", JSON.stringify(5000));
+// localStorage.setItem("balance", JSON.stringify(5000));
+const balance =  localStorage.getItem("balance");
+if(!balance){
+  localStorage.setItem("balance", JSON.stringify(5000));
+}
 function getExpences() {
   const storedExpenses = JSON.parse(localStorage.getItem("expenses"));
   return storedExpenses;
@@ -37,17 +43,25 @@ function ExpenseHome() {
   const [editMode, setEditMode] = useState(false);
   const [editExpenseId, setEditExpenseId] = useState(null);
   const [deleteFlag, setDeleteFlag] = useState(false);
-  const [editFlag, setEditFlag] = useState(false);
-
+  const [increaseFlag, setIncreaseFlag] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
   useEffect(() => {
+    
     localStorage.setItem("expenses", JSON.stringify(expenses));
-  }, [expenses]);
+    localStorage.setItem("balance", JSON.stringify(walletBalance));
+  }, [expenses,increaseFlag]);
 
   const increaseWalletAmount = (amount) => {
     setWalletBalance(walletBalance + Number(amount));
-    localStorage.setItem("balance", JSON.stringify(walletBalance));
+    setIncreaseFlag(true)
   };
   const addExpense = (expense) => {
+    
+    if(expenseAmount > walletBalance){
+      // alert("You have no sufficient balance")
+      enqueueSnackbar("You have no sufficient balance", { variant: "error" });
+      return;
+    }
     if (editMode) {
       const edittedData = expenses.map((elem) => {
         if (elem.id === editExpenseId) {
@@ -78,6 +92,8 @@ function ExpenseHome() {
     setExpenses(updatedExpenses);
     setWalletBalance(walletBalance + deletedExpense.amount);
     setDeleteFlag(true);
+    let deletedAmount = deletedExpense.amount;
+    setExpenseAmount(expenseAmount - deletedAmount);
   };
 
   const editExpense = (id) => {
@@ -109,6 +125,7 @@ function ExpenseHome() {
             editItem={editExpense}
             addExpense={addExpense}
           />
+                    <ExpenseTrend data={expenses} />
         </div>
       </div>
     </>
